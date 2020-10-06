e84849d1b947f9d7b6c47b8e9dc424a0c233b85e
TippieCodes <rowan@tippie.me>
2020-10-06 11:32:20 +0200

    Added checking if file exists in roster.message()

--- commands/eval.js
+++ commands/eval.js
@@ -8,13 +8,13 @@ module.exports = {
         try {
             const code = args.join(" ");
             let evaled = eval(code);
-
             if (typeof evaled !== "string")
                 evaled = require("util").inspect(evaled);
 
             message.channel.send(utils.clean(evaled), {code: "xl"}).then();
         } catch (err) {
             message.channel.send(`\`ERROR\` \`\`\`xl\n${utils.clean(err)}\n\`\`\``);
+            return;
         }
     },
 };
--- roster.js
+++ roster.js
@@ -3,9 +3,11 @@ module.exports.message = function(template, message) {
     fs.readdir("./templates/", (err, files) => {
         if (err) return console.error(err);
         template = template + '.js'
-        console.log(files)
-        let file = files.find(a => a = template)
-        console.log(file)
-        if(file != undefined) message.channel.send('This template has not been found!')
+        let file = files.find(a => a === template)
+        if(file === undefined) {
+            return message.channel.send("This template has not been found!") //throw new Error("This template has not been found!");
+        } else {
+        return file;
+        }
     })
 }
