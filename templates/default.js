b18f60ce378682626b79f39ebad79ae7c8c1876a
TippieCodes <rowan@tippie.me>
2020-10-07 14:32:33 +0200

    Rough finish of default.js

--- templates/default.js
+++ templates/default.js
@@ -1,18 +1,23 @@
-module.exports.construct = async function(message, roles) {
-    let result = undefined
-    let itemsProcessed = 0
-    console.log(roles)
-    JSON.parse("[" + roles + "]").forEach(element => async function(){
-        console.log(element)
-        let role = await message.guild.roles.fetch(element)
-        result = result + role.name + '\n'
-        role.members.forEach(member => {
-            itemsProcessed++;
-            result = result + member + '\n'
-            if(itemsProcessed === members.array.length) {
-                result = result + '\n'
+module.exports.construct = function(message, roles_wanted) {
+    return new Promise(async function(resolve, reject) {
+        let result = undefined
+        var roles_array = JSON.parse("[" + roles_wanted + "]")
+        console.log(roles_array)
+        var roles = await message.guild.roles.fetch()
+        var itemsProcessed = 0
+        let sub_result = ""
+        for (element of roles_array){
+            const role = await roles.cache.find(r => r.id == element)
+            sub_result =  sub_result + role.name + '\n'
+            for (member of role.members) {
+                sub_result = sub_result + '- <@' + member[1].id + '> \n'
+            }    
+            itemsProcessed++
+            sub_result = sub_result + '\n'
+            console.log(itemsProcessed + ' - ' + roles_array.length)
+            if (itemsProcessed === roles_array.length){
+                resolve(sub_result)
             }
-        })    
+        }
     });
-    return result
 }
