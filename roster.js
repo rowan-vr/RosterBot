fa8aaef0bf952ef644d634baf59f45938e4ec955
TippieCodes <rowan@tippie.me>
2020-10-08 08:34:16 +0200

    Changed function to promise function

--- roster.js
+++ roster.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
 module.exports.message = function(template, message, roles) {
-    fs.readdir("./templates/", (err, files) => {
-        if (err) return console.error(err);
-        template = template + '.js'
-        let file = files.find(a => a === template)
-        if(file === undefined) {
-            // throw new Error("This template has not been found!");
-            return message.channel.send("This template has not been found!") 
-        } else {
-        const template = require('./templates/' + file);
-        template.construct(message.guild, roles).then(m => {
-            message.channel.send(m)
-        })}
+    return new Promise(function(resolve,reject){
+        fs.readdir("./templates/", (err, files) => {
+            if (err) return reject(err);
+            template = template + '.js'
+            let file = files.find(a => a === template)
+            if(file === undefined) {
+                // throw new Error("This template has not been found!");
+                return reject("This template has not been found!") 
+            } else {
+            const template = require('./templates/' + file);
+            template.construct(message.guild, roles).then(m => {
+                resolve(m)
+            })}
+        })
     })
 }
