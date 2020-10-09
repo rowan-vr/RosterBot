f49e5aecbfcc02e44274eda27adad9f2d82c152c
TippieCodes <rowan@tippie.me>
2020-10-09 12:57:59 +0200

    Added multi-message system

--- commands/create.js
+++ commands/create.js
@@ -3,13 +3,30 @@ module.exports = {
     name: 'createroster',
     description: 'Create a roster',
     execute(client, message, args) {
-        console.log(args)
         if(args.length == 0) return message.channel.send("Please supply arguments, GUI is WIP")
         let template = args[0]
         let roles = args[1]
         if (template === undefined || roles === undefined) return message.channel.send("You did not supply the right arguments")
         roster.message(template, message.guild, roles).then(m => {
-            message.channel.send(m)
+            if (m.length > 1999){
+                let m_lines = m.split("\n")
+                var send_msg = ""
+                let i = 0
+                let processed = 0
+                m_lines.forEach(m_line => {
+                    send_msg += m_line + "\n";
+                    i++;
+                    processed++;
+                    if (i===20){
+                        message.channel.send(send_msg)
+                        send_msg = ""
+                        i = 0
+                    }
+                    if (processed === m_lines.length) message.channel.send(send_msg)
+                });
+            } else {
+                message.channel.send(m)
+            }
         }).catch(e =>{
             message.channel.send(`An error occured\n\`${e}\``)
         })
